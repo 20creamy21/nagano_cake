5ee9ac0da134a9dead234322e755eac5394610d0
EC2 Default User <ec2-user@ip-172-31-24-173.ec2.internal>
2021-04-02 14:03:25 +0000

    [Add] admin/genres,customers,orders,homesコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :admin do
+    root to: "homes#top"
+    resources :items, only:[:index, :new, :create, :show, :edit, :update]
+    resources :genres, only:[:index, :create, :edit, :update]
+    resources :customers, only:[:index, :show, :edit, :update]
+    resources :orders, only:[:update]
+  end
+
 end
