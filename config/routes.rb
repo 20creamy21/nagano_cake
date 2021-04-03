84a09b93f26f4ff2bc51e54a6d223a0c2344e77b
EC2 Default User <ec2-user@ip-172-31-24-173.ec2.internal>
2021-04-03 06:03:01 +0000

    [Update] deviseのルーティングを変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
   namespace :admin do
-    devise_for :admins
+    devise_scope :admins do
+      get    "sign_in"  => "/admin/sessions#new"
+      post   "sign_in"  => "/admin/sessions#create"
+      delete "sign_out" => "/admin/sessions#destroy"
+    end
     root to: "homes#top"
     resources :items, only:[:index, :new, :create, :show, :edit, :update]
     resources :genres, only:[:index, :create, :edit, :update]
