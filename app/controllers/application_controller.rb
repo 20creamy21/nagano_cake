605268d8d933a82b761b4114df41180e48341f33
EC2 Default User <ec2-user@ip-172-31-24-173.ec2.internal>
2021-04-04 05:08:43 +0000

    [Add] ログイン前はログイン画面意外にアクセス出来ないようにした

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::Base
 
+  before_action :authenticate_admin!
+
   def after_sign_in_path_for(resource)
     admin_root_path
+  end
+
+  def after_sign_out_path_for(resource)
+    admin_sign_in_path
   end
 
 end
