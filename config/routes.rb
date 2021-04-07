d10a7a3147dac2b222a46993461d92d72052a951
EC2 Default User <ec2-user@ip-172-31-24-173.ec2.internal>
2021-04-07 15:16:16 +0000

    [Add] アソシエーションの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
 
   devise_for :admin, controllers: {
-  sessions: 'admin/sessions'
+  sessions: 'admin/sessions',
+  registrations: 'admin/registrations',
+  passwords: 'admin/passwords'
   }
 
+  devise_for :customers, controllers: {
+  sessions: 'public/customers/sessions',
+  registrations: 'public/registrations',
+  passwords: 'public/passwords'
+  }
 
   namespace :admin do
     root to: "homes#top"
@@ -11,6 +18,16 @@ Rails.application.routes.draw do
     resources :genres, only:[:index, :create, :edit, :update]
     resources :customers, only:[:index, :show, :edit, :update]
     resources :orders, only:[:update]
+  end
+
+  namespace :public do
+    root to: "homes#top"
+    get '/about' => 'homes#about'
+    resources :items, only:[:index, :show]
+    resources :customers, only:[:show, :edit, :update, :unsubscribe, :withdraw]
+    resources :cart_items, only:[:index, :update, :destroy, :destroy_all, :create]
+    resources :orders, only:[:new, :comfirm, :complete, :create, :index, :show]
+    resources :addresses, only:[:index, :edit, :create, :update, :destroy]
   end
 
 end
