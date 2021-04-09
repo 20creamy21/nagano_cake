6e3798cc1be50cde4f3c178957fa9650444eb620
EC2 Default User <ec2-user@ip-172-31-24-173.ec2.internal>
2021-04-09 16:15:38 +0000

    [Fix] ルーティングの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
 
   before_action :authenticate_admin!,except: [:top]
+  
+  def after_sign_up_path_for(resource)
+    admin_root_path
+  end
 
   def after_sign_in_path_for(resource)
     admin_root_path
--- config/routes.rb
+++ config/routes.rb
@@ -31,13 +31,28 @@ Rails.application.routes.draw do
   # end
 
   scope module: :public do
+
     root to: "homes#top"
+
     get '/about' => 'homes#about'
+
     resources :items, only:[:index, :show]
-    resources :customers, only:[:show, :edit, :update, :unsubscribe, :withdraw]
+
+    get '/customers/my_page' => 'customers#show'
+    get '/customers/edit' => 'customers#edit'
+    patch '/customers' => 'customers#update'
+    get '/customers/unsubscribe' => 'customers#unsubscribe'
+    patch '/customers/withdraw' => 'customers#withdraw'
+
+    delete '/cart_items/destroy_all' => 'cart_items#destroy_all'
     resources :cart_items, only:[:index, :update, :destroy, :destroy_all, :create]
-    resources :orders, only:[:new, :comfirm, :complete, :create, :index, :show]
+
+    resources :orders, only:[:new, :create, :index, :show]
+    post '/orders/confirm' => 'orders#confirm'
+    get '/orders/complete' => 'orders#complete'
+
     resources :addresses, only:[:index, :edit, :create, :update, :destroy]
+
   end
 
 end
