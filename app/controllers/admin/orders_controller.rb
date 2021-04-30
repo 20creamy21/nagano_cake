7e43b936b2403c453d863cf85bae2d994769494b
EC2 Default User <ec2-user@ip-172-31-24-173.ec2.internal>
2021-04-30 14:32:58 +0000

    [Add] レイアウト追加

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,5 +1,10 @@
 class Admin::OrdersController < ApplicationController
-  
+
   before_action :authenticate_admin!, except: [:top]
-  
+
+  def show
+    @order = Order.find(params[:id])
+    @order_details = OrderDetail.where(order_id: @order.id)
+  end
+
 end
